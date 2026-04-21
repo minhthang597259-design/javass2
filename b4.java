349734f61f2e02a1599c97d6271223c1a9643851
minhthang597259-design <minhthang597259@gmail.com>
2026-04-21 11:55:00 +0700

    Update b4.java

--- b4.java
+++ b4.java
@@ -1,15 +1,30 @@
-package btvnss1;
 import java.util.Scanner;
-public class b4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a,b,c,d;
-        System.out.println("chieu dai : ");
-        a=sc.nextInt();
-        System.out.println("chieu rong : ");
-        b=sc.nextInt();
-        System.out.println("dien tich : " + (a*b));
-        System.out.println("chu vi : " + (a+b));
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập cạnh thứ nhất: ");
+        int a = scanner.nextInt();
+        System.out.print("Nhập cạnh thứ hai: ");
+        int b = scanner.nextInt();
+        System.out.print("Nhập cạnh thứ ba: ");
+        int c = scanner.nextInt();
+
+        if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) {
+            if (a == b && b == c) {
+                System.out.println("Loại tam giác: Tam giác đều");
+            } else if (a == b || a == c || b == c) {
+                System.out.println("Loại tam giác: Tam giác cân");
+            } else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
+                System.out.println("Loại tam giác: Tam giác vuông");
+            } else {
+                System.out.println("Loại tam giác: Tam giác thường");
+            }
+        } else {
+            System.out.println("Ba cạnh không tạo thành tam giác.");
+        }
+
+        scanner.close();
     }
 }
